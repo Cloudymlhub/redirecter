d6d7afb0f4731f835d58b4efd85ee0e4fb12614a
souley <souleymassly1@gmail.com>
2025-10-08 21:56:24 +0400

    wip

--- main.py
+++ main.py
@@ -34,4 +34,4 @@ async def redirect_mata_channel():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8081)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
